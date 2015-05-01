8de2ec9f5d58784af27d0fb311710d51759a77f4
mcarlile <mckenzie.carlile@gmail.com>
2015-04-30 18:09:24 -0700

    Included Colorduino library
    from http://blog.lincomatic.com/?p=148

--- Installation/LEDmap/LEDmap.ino
+++ Installation/LEDmap/LEDmap.ino
@@ -1,1 +1,23 @@
+#include <TinkerKit.h>
+#include <Colorduino.h>
+
+
+//taken from http://blog.lincomatic.com/?p=148
+
+void setup() {
+  Colorduino.Init();
+  // compensate for relative intensity differences in R/G/B brightness
+  // array of 6-bit base values for RGB (0~63)
+  // whiteBalVal[0]=red
+  // whiteBalVal[1]=green
+  // whiteBalVal[2]=blue
+  unsigned char whiteBalVal[3] = {
+    63,63,63    }; // for LEDSEE 6x6cm round matrix
+  Colorduino.SetWhiteBal(whiteBalVal);
+}
+
+void loop() {
+}
+
+
 
